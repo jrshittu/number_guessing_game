0c5ce1a5410e63a7cbafdd8462eb8cf38381f3c6
Abideen <misterjrshittu@gmail.com>
2023-07-04 20:33:55 +0100

    python program done

---
+++ number_guessing.py
@@ -0,0 +1,15 @@
+import random
+
+computer_choice = random.randint(1, 10)
+
+
+trials = 0
+
+while trials <= 10:
+    user_choice = int(input("Kindly make a choice between 1 and 10: "))
+    if user_choice == computer_choice:
+        print("Voilla😍")
+        break
+    else:
+        print("You 🤣")
+        
